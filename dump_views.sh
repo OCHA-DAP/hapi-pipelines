6901862a37bf4aac863ca4e9dfa9979782621c99
mcarans <rans@email.com>
2024-05-01 11:01:04 +1200

    Make changes from Monica Dump all tables Remove - from hash files USe subfolder for csv

--- dump_views.sh
+++ dump_views.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
-today=$(date +%Y%m%d-%H%M%S)
+printf -v now '%(%Y-%m-%d %H:%M:%S)T\n' -1
+echo "Started at $now"
 
-echo "Started at $today"
+mkdir -p database/csv
 
-mkdir -p csv_export
+tables=$(docker exec -t postgres-container psql -t -U postgres -d hapi -c \
+  "select table_name from INFORMATION_SCHEMA.views WHERE table_schema = ANY (current_schemas(false))")
 
-for table in location admin1 admin2 population operational_presence humanitarian_needs
+for table in $tables
 do
+  table=${table::-1}
+  if [[ -z "$table" ]]; then
+    continue
+  fi
+
   echo "Saving $table"
-  docker exec -t postgres-container psql -U postgres -d hapi -c "\copy (select * from ${table}_view) TO STDOUT (FORMAT csv, DELIMITER ',',  HEADER);" | tee >(gzip > csv_export/${table}.csv.gz) >(md5sum > csv_export/${table}.hash) >/dev/null
+
+  docker exec -t postgres-container psql -U postgres -d hapi -c \
+    "\copy (select * from ${table}_view) TO STDOUT (FORMAT csv, DELIMITER ',',  HEADER);" \
+    | tee \
+    >(gzip > database/csv/${table}.csv.gz) \
+    >(md5sum | awk '{print $1}' > database/csv/${table}.hash) \
+    >/dev/null
 done
 
 docker exec -t postgres-container pg_dump -U postgres -Fc hapi -f hapi_db.pg_restore
-docker cp postgres-container:/hapi_db.pg_restore csv_export/hapi_db.pg_restore
+docker cp postgres-container:/hapi_db.pg_restore database/hapi_db.pg_restore
 
-# end
-end=$(date +%Y%m%d-%H%M%S)
-
-echo "Ended at $end."
+printf -v now '%(%Y-%m-%d %H:%M:%S)T\n' -1
+echo "Ended at $now"
